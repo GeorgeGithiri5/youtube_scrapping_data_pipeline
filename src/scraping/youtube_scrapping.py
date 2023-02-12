1a47cca19ae3d9be1d4177ee44d99c8f2bfd2fef
GeorgeGithiri5 <georgegithiri002@gmail.com>
2023-02-12 14:00:17 +0300

    Feature: Tested Out CSV export

--- src/scraping/youtube_scrapping.py
+++ src/scraping/youtube_scrapping.py
@@ -12,7 +12,7 @@ DEVELOPER_KEY = "AIzaSyB32r77YhdQiheZicUEJA-2sQZXT1tIAgI"
 def query_youtube_search():
     session = HTMLSession()
     topics = ['climate', 'agriculture', 'health', 'economics']
-    url = "https://www.youtube.com/results?search_query=good+health"
+    url = "https://www.youtube.com/results?search_query=agriculture"
     response = session.get(url)
     response.html.render(sleep=5, timeout = 1000, keep_page = True, scrolldown = 10)
     
@@ -38,8 +38,15 @@ def get_youtube_video_ids(links_list):
     """
         Description: Get youtube video ids from links collected
     """
-    
+    video_ids = []
+    for url in links_list:
+        video_id = url.split("v=")[-1]
+        ampersand_position = video_id.find("&")
+        if ampersand_position != -1:
+            video_id = video_id[:ampersand_position]
+        video_ids.append(video_id)
         
+    return video_ids
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -48,21 +55,34 @@ def main():
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
-
-    request = youtube.commentThreads().list(
-        part="id, snippet",
-        videoId="P-dUmZOsdJ4"
-    )
-    response = request.execute()
-    response = response['items']
-    listing = []
+    video_ids = get_youtube_video_ids(links_list)
+    dataset = pd.DataFrame()
     
-    for item in response:
-        data = flatten(item)
-        listing.append(data)
+    for id in video_ids:
+        print(f"{id}")
+        request = youtube.commentThreads().list(
+            part="id, snippet",
+            videoId= f"{id}"
+        )
+        response = request.execute()
+        
+        response = response['items']
+        listing = []
+        
+        for item in response:
+            is_public = item['snippet']['isPublic']
+            if not is_public:
+                print("This comment is restricted and not publicly visible.")
+            else:
+                data = flatten(item)
+                listing.append(data)
 
-    dataframe = pd.DataFrame(listing)
-    dataframe.to_csv("data.csv")
+        dataframe = pd.DataFrame(listing)
+        dataset = dataset.append(dataframe)
+        print(dataframe.shape)
+        print(dataset.shape)
+    
+    dataset.to_csv("data.csv")
     print("exported")
 
 if __name__ == "__main__":
