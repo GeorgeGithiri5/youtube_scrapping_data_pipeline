5a191de00c2d98012fa516d35a2e2eaea494701e
GeorgeGithiri5 <georgegithiri002@gmail.com>
2023-02-14 22:39:19 +0300

    Load Data to redshift

--- src/aws-glue-jobs/youtube_comments_s3.py
+++ src/aws-glue-jobs/youtube_comments_s3.py
@@ -22,10 +22,35 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 def load_data_to_redshift(dyn_df: DynamicFrame, host: str, port: int, user: str, pwd: str, table: str,args, glueContext):
-    
+    redshift_conn_options = {
+        "url": f"jdbc:redshift://{host}:{port}/{REDSHIFT_DATABASE_NAME}",
+        "dbtable": table,
+        "user": user,
+        "password": pwd,
+        "redshiftTmpDir": args["TempDir"],
+    }
+
+    glueContext.write_dynamic_frame.from_options(
+        frame=dyn_df,
+        connection_type="redshift",
+        connection_options=redshift_conn_options,
+    )
 
 def main():
     data = youtube_data
+    sdf = spark.createDataFrame(data)
+    dyn_df = DynamicFrame.fromDF(sdf, glueContext, "dyn_df")
+    
+    load_data_to_redshift(
+        dyn_df,
+        redshift_secret["host"],
+        redshift_secret["port"],
+        redshift_secret["username"],
+        redshift_secret["password"],
+        "tablename",
+        args,
+        glueContext,
+    )
     
 
 if __name__ == "__main__":
